71493cf838769c052537cb03375d0db84ad607fd
Nazarii Lysak <nazarlv87@gmail.com>
2024-12-01 23:45:25 +0200

    add swagger to progest

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -15,6 +15,7 @@ export class AppController {
       <h1>Uni-Space Server | Port ${process.env.SERVER_PORT}</h1>
       <hr />
       <p>Front-end Client: <a href="${process.env.FRONTEND_URL}" target="_blank">${process.env.FRONTEND_URL}</a></p>
+      <p>Swagger docs: <a href="${process.env.SWAGGER_URL}" target="_blank">${process.env.SWAGGER_URL}</a></p>
     </body>
     </html>`;
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 import { AppModule } from './app.module';
@@ -6,9 +7,16 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+  .setTitle('Markets example')
+  .setDescription('The Markets API description')
+  .setVersion('1.0')
+  .addTag('Markets')
+  .build();
+const documentFactory = () => SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(process.env.SERVER_PORT ?? 3000);
 }
